b18635b1bd165d6ab40c8e5ca178ef447c6f62e0
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2022-11-16 13:52:57 -0300

    Update EventsCall.cs
    -events comentado

--- Assets/Script/Menus/EventsCall.cs
+++ Assets/Script/Menus/EventsCall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Events;
+//using UnityEditor.Events;
 using TMPro;
 
 public class EventsCall : MonoBehaviour
