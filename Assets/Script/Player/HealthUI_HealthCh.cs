59bdd388bc75b3ff83d2e2d8d19f19fcbad4ddc5
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2022-11-30 07:49:42 -0300

    feedback danio del jugador

--- Assets/Script/Player/HealthUI_HealthCh.cs
+++ Assets/Script/Player/HealthUI_HealthCh.cs
@@ -1,22 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class HealthUI_HealthCh : HealthCh_LogicActive
 {
 
     float hpPercentage = 1;
-
+    PostProcessVolume volume;
+    Vignette vignette;
     void RefreshHealth()
     {
         Sliders.SrchSlider("Health").CurrValue(hpPercentage);
     }
 
-    private void Start()
-    {
-        RefreshHealth();
-    }
-
+   
     public override void Activate(params float[] floatParms)
     {
         if (floatParms[1] <= 0)
@@ -26,6 +24,33 @@ public class HealthUI_HealthCh : HealthCh_LogicActive
 
         hpPercentage = floatParms[floatParms.Length - 1];
 
+        vignette.intensity.Override(0.9f);
+
         RefreshHealth();
+    }
+
+    private void Start()
+    {
+        RefreshHealth();
+        vignette = ScriptableObject.CreateInstance<Vignette>();
+        vignette.enabled.Override(true);
+        vignette.intensity.Override(0);
+        vignette.color.Override(Color.red);
+        vignette.smoothness.Override(0.3f);
+        vignette.roundness.Override(1);
+        vignette.rounded.Override(true);
+
+        volume = PostProcessManager.instance.QuickVolume(12, 1, vignette);
+    }
+
+
+    private void Update()
+    {
+        vignette.intensity.Override(Mathf.Lerp(vignette.intensity.value, 0.6f-hpPercentage*0.6f, Time.deltaTime));
+    }
+
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(volume, true, true);
     }
 }
